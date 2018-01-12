a48e62b66a1295443536d48bfc557cca3916d046
Anwesh43 <anweshthecool0@gmail.com>
2018-01-12 08:33:19 +0530

    adding CircleExpandView

--- circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
+++ circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
@@ -3,3 +3,15 @@ package ui.anwesome.com.circleexpandview
 /**
  * Created by anweshmishra on 12/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class CircleExpandView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        return true
+    }
+}
