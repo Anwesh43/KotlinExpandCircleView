82831f4a99904e2e720846dddad37ab054f1f5a0
Anwesh43 <anweshthecool0@gmail.com>
2018-01-12 20:31:33 +0530

    adding CircleExpandState

--- circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
+++ circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
@@ -30,4 +30,17 @@ class CircleExpandView(ctx:Context):View(ctx) {
 
         }
     }
+    data class CircleExpandState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            dir = 1-2*scale
+        }
+    }
 }
