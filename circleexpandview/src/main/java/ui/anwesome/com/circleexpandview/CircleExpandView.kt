bbf8ab9866641020e7b523f99d579d276fa0b1e5
Anwesh43 <anweshthecool0@gmail.com>
2018-01-12 08:38:32 +0530

    adding Circle

--- circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
+++ circleexpandview/src/main/java/ui/anwesome/com/circleexpandview/CircleExpandView.kt
@@ -14,4 +14,20 @@ class CircleExpandView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
     }
+    data class CircleExpand(var x:Float,var y:Float,var r:Float,var or:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.style = Paint.Style.FILL
+            paint.strokeWidth = 8f
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawCircle(0f,0f,or+(r-or),paint)
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
